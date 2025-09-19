f4c7e774a0213a648944ec9dd09057ef9e6dc892
Musfique Ahmed <mahmed2330101@bsds.uiu.ac.bd>
2025-09-19 20:22:12 +0600

    sadf

--- Class_13/my_app.py
+++ Class_13/my_app.py
@@ -5,4 +5,22 @@ root = tk.Tk()
 root.title("My First GUI")
 root.geometry("600x400")
 
+label = tk.Label(root, text= " Hello, How are you? ")
+label.pack()
+
+title_label = tk.Label(
+            root,
+            text="Retail Billing System",
+            bd=12,
+            relief=tk.GROOVE,
+            bg="white",
+            fg="red",
+            font=("Arial", 30, "bold"),
+            pady=2
+        )
+title_label.pack(fill=tk.X)
+
+
 root.mainloop()
+
+
