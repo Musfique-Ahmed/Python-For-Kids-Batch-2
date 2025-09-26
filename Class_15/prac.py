ef2202463fdb59bd83648136b1338db9a4cfeb7c
Musfique Ahmed <mahmed2330101@bsds.uiu.ac.bd>
2025-09-26 20:11:00 +0600

    Update prac.py

--- Class_15/prac.py
+++ Class_15/prac.py
@@ -1,11 +1,60 @@
+# import turtle
+# screen= turtle.Screen()
+# t=turtle.Turtle()
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+# turtle.done()
+
+# import turtle
+
+# screen = turtle.Screen() 
+# t = turtle.Turtle()       
+# for _ in range(4):
+#     t.forward(100)  
+#     t.right(90)     
+
+# turtle.done()
+
+
+# import turtle
+
+# screen = turtle.Screen() 
+# t = turtle.Turtle()       
+# for _ in range(3):
+#     t.forward(100)  
+#     t.right(120)     
+
+# turtle.done()
+
 import turtle
 screen= turtle.Screen()
 t=turtle.Turtle()
-t.forward(100)
-t.right(90)
-t.forward(100)
-t.right(90)
-t.forward(100)
-t.right(90)
-t.forward(100)
+
+
+t.forward(50)
+t.right(45)
+t.forward(50)
+
+t.right(45)
+t.forward(50)
+
+t.right(45)
+t.forward(50)
+
+t.right(45)
+t.forward(50)
+
+t.right(45)
+t.forward(50)
+
+t.right(45)
+t.forward(50)
+
+t.right(45)
+t.forward(50)
 turtle.done()
